8c83a207f0bd62ff07954d2dc1fd383ca0790073
smokewheateveryday <2135@estudantes.ips.pt>
2019-10-18 12:33:18 +0100

    Added script that installs all missing dependencies when run. Hopefully it works

---
+++ dependency_install.R
@@ -0,0 +1,25 @@
+#Dependency installer script for Biome-shiny
+
+#Functions that check if packages are installed and if not, install them
+package_check_and_install = function(packages = c("package1","package2")){
+  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(packages, rownames(installed.packages())))
+  }
+}
+package_check_and_install_bioconductor = function(packages = c("package1","package2")){
+  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    BiocManager::install(setdiff(packages, rownames(installed.packages())))
+  }
+}
+
+#Packages installed through CRAN
+
+package_check_and_install(c("shiny","shinydashboard","dplyr","knitr","rmarkdown","DT",
+                            "ggplot2","ggpubr","hrbrthemes","ggplotify","RColorBrewer",
+                            "plotly","reshape2","vegan","abundant","BiocManager"))
+
+#BioConductor install
+BiocManager::install()
+
+#Packages installed by Bioconductor
+package_check_and_install_bioconductor(c("phyloseq","microbiome","biomformat"))
